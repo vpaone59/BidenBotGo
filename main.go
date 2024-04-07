6d1c78be2443c62ddf50d56fcb4b3b09d86627ba
VINCENT <55220731+vpaone59@users.noreply.github.com>
2024-04-07 11:39:26 -0400

    added a different way to do .env

--- main.go
+++ main.go
@@ -4,46 +4,30 @@ import (
 	"io"
 	"log"
 	"log/slog"
+	"math/rand"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/spf13/viper"
+	"github.com/joho/godotenv"
 )
 
-// Config stores all configuration of the application.
-// The values are read by viper from a config file or environment variable.
-type Config struct {
-	DiscordToken string `mapstructure:"DISCORD_TOKEN"`
-}
-
-// LoadConfig reads configuration from file or environment variables.
-func LoadConfig(path string) (config Config, err error) {
-
-	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
-	viper.SetConfigType("env")
-	viper.AutomaticEnv()
-
-	err = viper.ReadInConfig()
-	if err != nil {
-		return
-	}
-
-	err = viper.Unmarshal(&config)
-	return
-}
+const prefix string = ".."
 
 func main() {
+	godotenv.Load() // defaults to .env file
+	DISCORD_TOKEN := os.Getenv("DISCORD_TOKEN")
+
 	logFile := "app.log"
 	file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
-
 	defer file.Close()
 
+	// Write to Stdout and the log file
 	w := io.MultiWriter(os.Stderr, file)
 
 	handlerOpts := &slog.HandlerOptions{
@@ -51,30 +35,62 @@ func main() {
 	}
 	logger := slog.New(slog.NewJSONHandler(w, handlerOpts))
 	slog.SetDefault(logger)
-
-	// load app.env file data to struct
-	config, err := LoadConfig(".")
-	// handle errors
-	if err != nil {
-		log.Fatalf("can't load environment app.env: %v", err)
-	}
-
-	slog.Info(" -----Reading Environment variables Using Viper package----\n")
-	slog.Info("\nApplication_Environment = " + config.DiscordToken)
+	slog.Info("\nApplication_Environment = ")
 
 	// Bot setup
-	sess, err := discordgo.New("Bot " + config.DiscordToken)
+	sess, err := discordgo.New("Bot " + DISCORD_TOKEN)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		args := strings.Split(m.Content, " ")
+
 		if m.Author.ID == s.State.User.ID {
 			return
 		}
-
-		if m.Content == "biden" {
+		if args[0] != prefix {
+			return
+		}
+		if strings.Contains(m.Content, "biden") {
 			s.ChannelMessageSend(m.ChannelID, "blast")
+		}
+		if args[1] == "proverbs" {
+			proverbs := []string{
+				"Don't communicate by sharing memory, share memory by communicating.",
+				"Concurrency is not parallelism.",
+				"Channels orchestrate; mutexes serialize.",
+				"The bigger the interface, the weaker the abstraction.",
+				"Make the zero value useful.",
+				"interface{} says nothing.",
+				"Gofmt's style is no one's favorite, yet gofmt is everyone's favorite.",
+				"A little copying is better than a little dependency.",
+				"Syscall must always be guarded with build tags.",
+				"Cgo must always be guarded with build tags.",
+				"Cgo is not Go.",
+				"With the unsafe package there are no guarantees.",
+				"Clear is better than clever.",
+				"Reflection is never clear.",
+				"Errors are values.",
+				"Don't just check errors, handle them gracefully.",
+				"Design the architecture, name the components, document the details.",
+				"Documentation is for users.",
+				"Don't panic.",
+			}
+
+			selection := rand.Intn(len(proverbs))
+			slog.Info("prov", "proverb", proverbs[selection])
+
+			author := discordgo.MessageEmbedAuthor{
+				Name: "Rob Pike",
+				URL:  "https//go-proverbs.github.io",
+			}
+			embed := discordgo.MessageEmbed{
+				Title:  proverbs[selection],
+				Author: &author,
+			}
+			s.ChannelMessageSendEmbed(m.ChannelID, &embed)
+			s.ChannelMessageSend(m.ChannelID, proverbs[selection])
 		}
 	})
 
